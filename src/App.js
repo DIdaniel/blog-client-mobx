9289d76efc2d8cf6e990fe96326a1fcfaae9f87e
dream_Founus <cappu159@gmail.com>
2021-04-26 18:17:22 +0900

    news api information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Counter from './components/Counter';
 
+// https://newsapi.org/
+// https://newsapi.org/docs/get-started
+// https://newsapi.org/docs/get-started#top-headlines
+
+// API : d7bb277ef9b4433f8e13203fc2c46aa3
+// GET : https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
+
 
 function App() {
   return (
