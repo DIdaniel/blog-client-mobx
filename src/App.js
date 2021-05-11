2f8fb22aff6f27ddae4be5119ecf73a64d4ed183
김동일 <patrick@gimdong-il-ui-MacBookPro.local>
2021-05-12 08:54:18 +0900

    header & nav section css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-//import Counter from './components/Counter';
-import Axios from "axios";
-import Card from "./components/card section/Card";
+// import axios from "axios";
+import Login from "./components/login/Login";
+import Navbar from "./components/navigationbar/Navbar";
 import Header from "./components/header/Header";
 import Signup from "./components/signup/Signup";
-import Login from "./components/login/Login";
-
-// https://newsapi.org/docs/get-started
-// GET : https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
-
-const API = "d7bb277ef9b4433f8e13203fc2c46aa3";
+import Card from "./components/card-section/Card";
 
 function App() {
   const [newsUrl, setNewsUrl] = useState([]);
-
-  useEffect(() => {
-    Axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API}`)
-      .then((res) => setNewsUrl(res.data.articles[0]))
-      .then(console.log(newsUrl))
-      .catch((err) => console.log(err));
-  }, []);
-
-  // useEffect(async () => {
-  //   try {
-  //     const response = await Axios.get(
-  //       `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API}`
-  //     )
-  //       .then((res) => setNewsUrl(res.data.articles[0]))
-  //       .then(console.log(newUrl));
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }, []);
 
   return (
     <div className="App">
@@ -54,11 +30,3 @@ function App() {
 }
 
 export default App;
-
-// useEffect(() => {
-//   const fetchNewsData = async() => {
-//     const result = await Axios.get(url);
-//     return result
-//   }
-//   const result = fetchNewsData
-// }, [])
