7c1ffd6b75d0c646b0fc54bcc7ee8f49fe4cf0ed
Hoon <shoh@help-me.kr>
2021-05-20 11:00:00 +0900

    change order of header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 // import axios from "axios";
 import Login from "./components/login/Login";
-import Navbar from "./components/navigationbar/Navbar";
-
 import Signup from "./components/signup/Signup";
-import Card from "./components/card-section/Card";
 import Header from "./components/pages/common/header/Header";
 
 function App() {
   return (
     <>
-      <Header />
       <Switch>
         <Route exact path="/signup">
           <Signup />
@@ -21,6 +17,7 @@ function App() {
           <Login />
         </Route>
       </Switch>
+      <Header />
     </>
   );
 }
